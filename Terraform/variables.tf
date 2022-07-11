7c7915a79e56f666b6c997fc4b5bea354222f0bc
Xavier Tintin <79267255+alextintin007@users.noreply.github.com>
2022-07-11 16:47:04 -0500

    Create GKE cluster
    Create only the cluster for personal GCP

---
+++ Terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  default = "cluster-terraform-2"
+}
+
+variable "project" {
+  default = "cern-cms-epn"
+}
+
+variable "location" {
+  default = "us-central1-c"
+}
+
+variable "initial_node_count" {
+  default = 1
+}
+
+variable "machine_type" {
+  default = "e2-standard-4"
+}
